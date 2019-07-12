8897ba6ef0a68b6791fd5e798d415bff1e4d8465
Luke Watts <luke@snyk.io>
2019-07-12 12:00:51 +0100

    feat: removes npm update prompt

--- test/updater.test.js
+++ test/updater.test.js
@@ -1,6 +1,7 @@
 const tap = require('tap');
 const test = tap.test;
 const updateCheck = require('../src/lib/updater').updateCheck;
+const fs = require('fs');
 const path = require('path');
 const sinon = require('sinon').createSandbox();
 const updateNotifier = require('update-notifier');
@@ -8,7 +9,7 @@ const updateNotifier = require('update-notifier');
 // Fake location of the package.json file and verify the code behaves well
 test('missing package.json', (t) => {
   t.plan(1);
-  let resolveStub = sinon.stub(path, 'resolve');
+  const resolveStub = sinon.stub(path, 'resolve');
   resolveStub.onFirstCall().returns('falseDir');
   resolveStub.onSecondCall().returns('falseFile');
 
@@ -20,11 +21,21 @@ test('missing package.json', (t) => {
   t.end();
 });
 
+test('STANDALONE declaration present', (t) => {
+  const fsStub = sinon.stub(fs, 'existsSync').callsFake(() => true);
+  t.tearDown(() => {
+    fsStub.restore();
+  });
+
+  t.equal(updateCheck(), false, 'Notifier was not started for binary build');
+  t.end();
+});
+
 // Run updateNotifier API for the basic package. THe target is to verify API still stands
 test('verify updater', (t) => {
-   var pkg = {
+  const pkg = {
     name: 'snyk',
-    version: '1.0.0'
+    version: '1.0.0',
   };
   const notifier = updateNotifier({pkg});
 
