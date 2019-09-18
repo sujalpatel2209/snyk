9341783bc0c6f2830cf7e294fae978b21c680c2f
Luke Watts <luke@snyk.io>
2019-09-18 15:10:26 +0200

    test: stub exists sync

--- test/updater.test.js
+++ test/updater.test.js
@@ -2,19 +2,17 @@ const tap = require('tap');
 const test = tap.test;
 const updateCheck = require('../src/lib/updater').updateCheck;
 const fs = require('fs');
-const path = require('path');
 const sinon = require('sinon').createSandbox();
 const updateNotifier = require('update-notifier');
 
 // Fake location of the package.json file and verify the code behaves well
 test('missing package.json', (t) => {
-  t.plan(1);
-  const resolveStub = sinon.stub(path, 'resolve');
-  resolveStub.onFirstCall().returns('falseDir');
-  resolveStub.onSecondCall().returns('falseFile');
+  const fsStub = sinon.stub(fs, 'existsSync');
+  fsStub.onFirstCall().returns(true);
+  fsStub.onSecondCall().returns(false);
 
   t.tearDown(() => {
-    resolveStub.restore();
+    fsStub.restore();
   });
 
   t.equal(updateCheck(), false, 'Notifier was not started on missing package.json');
